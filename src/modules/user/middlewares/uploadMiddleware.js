b267e0c709a1e073cf6403fd5762f33b2b163dc1
Pankaj Sanam <pankaj.sanam@gmail.com>
2021-02-07 14:25:55 +0530

    Added a upload middleware and removed it from user route

---
+++ src/modules/user/middlewares/uploadMiddleware.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, path.join(__dirname, '../../../public/uploads/'));
+  },
+  filename: (req, file, callback) => {
+    callback(null, `${Date.now()}-${file.originalname}`);
+  }
+});
+
+const uploadMiddleware = multer({
+  storage,
+  limits: {
+    fileSize: 4 * 1024 * 1024 // 4 MB
+  }
+});
+
+module.exports = uploadMiddleware;
--- src/modules/user/routes/user.route.js
+++ src/modules/user/routes/user.route.js
@@ -1,47 +1,31 @@
 const express = require('express');
-const path = require('path');
-const multer = require('multer');
-const auth = require('../middlewares/authMiddleware');
-const validate = require('../middlewares/validateMiddleware');
+
+const authMiddleware = require('../middlewares/authMiddleware');
+const validateMiddleware = require('../middlewares/validateMiddleware');
 const userValidation = require('../validations/user.validation');
 const userController = require('../controllers/user.controller');
+const uploadMiddleware = require('../middlewares/uploadMiddleware');
 
 const router = express.Router();
 
 router
   .route('/logout/:refreshToken')
-  .delete(auth('login'), userController.logoutUser);
+  .delete(authMiddleware('login'), userController.logoutUser);
 
 router
   .route('/')
-  .post(auth('manageUsers'), validate(userValidation.createUser), userController.createUser)
-  .get(auth('getUsers'), validate(userValidation.getUsers), userController.getUsers);
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, path.join(__dirname, '../../../public/uploads/'));
-  },
-  filename: (req, file, callback) => {
-    callback(null, `${Date.now()}-${file.originalname}`);
-  }
-});
-
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5242880
-  }
-});
+  .post(authMiddleware('manageUsers'), validateMiddleware(userValidation.createUser), userController.createUser)
+  .get(authMiddleware('getUsers'), validateMiddleware(userValidation.getUsers), userController.getUsers);
 
 router
   .route('/profile')
-  .get(auth('manageUsers'), userController.getProfile)
-  .put(auth('manageUsers'), upload.single('avatar'), userController.updateProfile);
+  .get(authMiddleware('manageUsers'), userController.getProfile)
+  .put(authMiddleware('manageUsers'), uploadMiddleware.single('avatar'), userController.updateProfile);
 
 router
   .route('/:userId')
-  .get(auth('getUsers'), validate(userValidation.getUser), userController.getUser)
-  .patch(auth('manageUsers'), validate(userValidation.updateUser), userController.updateUser)
-  .delete(auth('manageUsers'), validate(userValidation.deleteUser), userController.deleteUser);
+  .get(authMiddleware('getUsers'), validateMiddleware(userValidation.getUser), userController.getUser)
+  .patch(authMiddleware('manageUsers'), validateMiddleware(userValidation.updateUser), userController.updateUser)
+  .delete(authMiddleware('manageUsers'), validateMiddleware(userValidation.deleteUser), userController.deleteUser);
 
 module.exports = router;
